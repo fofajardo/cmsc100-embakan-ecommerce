ecaa870e0f931148f0ba7dc729535be77ea691f2
fofajardo <fofajardo@up.edu.ph>
2023-11-28 00:22:49 +0800

    chore: remove placeholder controller and router code.
    This also moves the CORS modifier into its own file and database handling back to index.js.

---
+++ api/corsModifier.js
@@ -0,0 +1,9 @@
+function corsModifier(aRequest, aResponse, aNext) {
+    aResponse.setHeader("Access-Control-Allow-Origin", "*");
+    aResponse.setHeader("Access-Control-Allow-Credentials", "true");
+    aResponse.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    aResponse.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    aNext();
+}
+
+export default corsModifier;
--- api/index.js
+++ api/index.js
@@ -1,16 +1,22 @@
 import express from "express";
-import router from "./router.js";
+import mongoose from "mongoose";
 
-// Constants.
+import corsModifier from "./corsModifier.js";
+
+const kDbHost = "127.0.0.1";
+const kDbPort = "27017";
+const kDbName = "aetos-commerce";
+const kDbString = `mongodb://${kDbHost}:${kDbPort}/${kDbName}`;
 const kPort = 3001;
+
 const gApp = express();
+
+await mongoose.connect(kDbString);
 
 // Use appropriate parsers to access the request/response body directly.
 gApp.use(express.json());
 gApp.use(express.urlencoded({ extended: false }));
-
-// Set up the router, which handles the endpoints.
-router(gApp);
+gApp.use(corsModifier);
 
 // Listen to the specified port.
 gApp.listen(kPort);
