f0a8e6199ae5674da26b9f277e3ae8cb67942a12
bkvdelfino <bvdelfino@up.edu.ph>
2023-12-16 14:56:15 +0800

    refactor: changed 'props' to 'prop' since only one product will be passed.

--- src/pages/ProductDetailView.js
+++ src/pages/ProductDetailView.js
@@ -1,10 +1,10 @@
 import * as React from 'react'; 
 import { Grid, Typography, Divider, Button, Box } from "@mui/material";
 
-export default function ProductDetailView(props){
+
+export default function ProductDetailView(prop){
     return {
         
-
 
     }
 }
