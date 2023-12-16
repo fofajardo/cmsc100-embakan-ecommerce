6a520b2ac07e0460f71c90cdb75e60dc797ad5ae
bkvdelfino <bvdelfino@up.edu.ph>
2023-12-16 18:27:06 +0800

    chore: transferred test cases from index.js to ProductDetailView.js.

--- src/pages/ProductDetailView.js
+++ src/pages/ProductDetailView.js
@@ -1,7 +1,38 @@
-// import { Grid, Typography, Divider, Button, Box } from "@mui/material";
+import React from 'react';
+import { Grid, Typography, Divider, Button, Box } from "@mui/material";
+
+// crop product test case for product detail view.
+const crop_product = {
+    ID: 1,
+    Name: "Bitter Melon (Ampalaya)",
+    Type: "Crop",
+    Description: "Harvested from Benguet.",
+    Price: 96,
+    Stock: 50,
+    Variant: "KG (4-5 pcs)",
+    Quantity: 1,
+    Image:"./components/products/crops/Ampalaya.jpg"
+};
+
+// poultry product test case for product detail view.
+const poultry_product = {
+    ID: 2,
+    Name: " Whole Chicken",
+    Type: "Poultry",
+    Description: "Born and raised from Bacolod.", 
+    Price: 175,
+    Stock: 50,
+    Variant: "Whole",
+    Quantity: 1,
+    Image:"./components/products/poultry/chicken.png"
+};
 
 export default function ProductDetailView(){
     return (
-        <h1>Hello</h1>
+        <Grid container spacing = {2}>
+            <Grid item sx = {8}>
+                <Typography>{ crop_product.Name }</Typography>
+            </Grid>
+        </Grid>
     )
 }
