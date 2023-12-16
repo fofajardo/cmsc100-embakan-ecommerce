1c4c4e5e8f28b77d7faa290453906d63bae571b7
bkvdelfino <bvdelfino@up.edu.ph>
2023-12-16 09:56:20 +0800

    test: added test cases (product entries) to test product detail view.

--- src/index.js
+++ src/index.js
@@ -12,6 +12,31 @@ import ProductsPublic from "./pages/ProductsPublic.js";
 import ManageProductsList from "./pages/manage/products/page.js"
 import ManageProductsCreate from "./pages/manage/products/create/page.js"
 import ManageProductsEdit from "./pages/manage/products/edit/page.js"
+import ProductDetailView from "./pages/ProductDetailView.js";  
+
+// crop product test case for product detail view.
+const crop_product = {
+    name: "Kangkong (Water Spinach)",
+    description: "Harvested from the province of Laguna, this is great for soup dishes like sinigang.",
+    type: "Crop",
+    price: 25,
+    stock: 50,
+    quantity: 1,
+    variant: "tali",
+    image:"./components/products/crops/kangkong.png"
+}
+
+// poultry product test case for product detail view.
+const poultry_product = {
+    name: " Whole Chicken",
+    description: "Raised in the stress-free environment of Bacolod, this poultry is great for dishes such as tinola, inasal, and more.",
+    type: "poultry",
+    price: 175,
+    stock: 50,
+    quantity: 1,
+    variant: "chicken",
+    image:"./components/products/poultry/chicken.png"
+}
 
 const router = createHashRouter([
     // { path: "/",         element: <Root /> },
