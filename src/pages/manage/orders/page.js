a14cc2bc2c6a11c1795429e2a73062f9a31bac42
fofajardo <fofajardo@up.edu.ph>
2024-01-02 00:59:13 +0800

    feat: fully implement and link order fulfillment.

--- src/pages/manage/orders/page.js
+++ src/pages/manage/orders/page.js
@@ -1,221 +1,210 @@
-import React, { useState } from "react";
-import { Container, Table, TableBody, TableCell, TableHead, TableRow, Typography, Box, Button } from "@mui/material";
+import React, { useEffect, useState } from "react";
 
-// import api from "../../apiGlue.js";
+import { useSnackbar } from "notistack";
 
-// const kBaseUrl = `${api.host}orders/`;
+import {
+    Container, Typography, Box, Grid, Stack, colors,
+    Button,
+    Card, CardActions, CardContent, CardHeader, Avatar
+} from "@mui/material";
 
-const testCases = [
-    {
-        productId: "123",
-        variantId: "456",
-        quantity: 5,
-        price: 50.00,
-        userId: "789",
-        status: 0, // 0 for 'pending'
-        date: new Date()
-    },
-    {
-        productId: "456",
-        variantId: "789",
-        quantity: 3,
-        price: 30.50,
-        userId: "987",
-        status: 2, // 2 for 'accomplished'
-        date: new Date()
-    },
-    {
-        productId: "789",
-        variantId: "012",
-        quantity: 2,
-        price: 20.00,
-        userId: "654",
-        status: 1, // 1 for 'canceled'
-        date: new Date()
-    },
-    {
-        productId: "234",
-        variantId: "567",
-        quantity: 1,
-        price: 15.75,
-        userId: "321",
-        status: 0, // 0 for 'pending'
-        date: new Date()
-    },
-    {
-        productId: "890",
-        variantId: "123",
-        quantity: 4,
-        price: 40.00,
-        userId: "456",
-        status: 2, // 2 for 'accomplished'
-        date: new Date()
-    },
-    {
-        productId: "567",
-        variantId: "890",
-        quantity: 2,
-        price: 25.50,
-        userId: "012",
-        status: 1, // 1 for 'canceled'
-        date: new Date()
-    },
-    {
-        productId: "123",
-        variantId: "456",
-        quantity: 3,
-        price: 35.25,
-        userId: "789",
-        status: 0, // 0 for 'pending'
-        date: new Date()
-    },
-    {
-        productId: "456",
-        variantId: "789",
-        quantity: 1,
-        price: 10.00,
-        userId: "987",
-        status: 2, // 2 for 'accomplished'
-        date: new Date()
-    },
-    {
-        productId: "789",
-        variantId: "012",
-        quantity: 5,
-        price: 55.50,
-        userId: "654",
-        status: 1, // 1 for 'canceled'
-        date: new Date()
-    },
-    {
-        productId: "234",
-        variantId: "567",
-        quantity: 2,
-        price: 20.75,
-        userId: "321",
-        status: 0, // 0 for 'pending'
-        date: new Date()
-    }
-];
+import {
+    HourglassEmpty as HourglassEmptyIcon,
+    Check as CheckIcon,
+    NotInterested as NotInterestedIcon,
+    Place as PlaceIcon,
+    List as ListIcon
+} from "@mui/icons-material";
 
-// async function put(aUrl, aData, aEnqueue, aSuccessMessage) {
-//   if (!aData) {
-//       aData = {};
-//   }
+import api from "../../apiGlue.js";
 
-//   return await base(aUrl, {
-//       method: "PUT",
-//       headers: {
-//           "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(aData)
-//   }, aEnqueue, aSuccessMessage);
-// }
-
-//Function for the canceling (put function containng the update of status)
-async function toCancel (){
-    /*
-    const data = {
-        status: formJson[0],
-    };
-    const productResult = await api.put(
-        `${kBaseUrl}${id}`,
-        data,
-        enqueueSnackbar,
-        "Transaction Cancelled.");
-    */
-}
-//Function for the confirming (put function containng the update of status)
-async function toConfirm (){
-    /*
-    const data = {
-        status: formJson[1],
-    };
-    const productResult = await api.put(
-        `${kBaseUrl}${id}`,
-        data,
-        enqueueSnackbar,
-        "Transaction Confirmed.");
-    */
-}
+const kBaseUrl = `${api.host}orders/`;
 
 export default function ManageOrders() {
+    const { enqueueSnackbar } = useSnackbar();
 
-    const [orders /*, setOrders*/] = useState(testCases);
-    // const orderLists = setOrders(testCases);
+    const [orders, setOrders] = useState([]);
+    const [update, setUpdate] = useState(0);
 
-    // const orderLists = fetch(kBaseUrl,
-    //   {
-    //       method: "GET",
-    //   }
-    // ).then(response => response.json()
-    // ).then(body => setOrders(body.data));
+    useEffect(function() {
+        api.get(kBaseUrl, enqueueSnackbar).then(function(aResponse) {
+            console.log(aResponse.data);
+            setOrders(aResponse.data);
+        });
+    }, [update]);
 
+    const handleOrdersChild = function(aOrder, aIndex) {
+        const { id, product, quantity, price, status } = aOrder;
+        const variant = product.variants.find(function(aElement) {
+            return (aElement.id == aOrder.variantId);
+        });
+        var statusInfo = {};
+
+        switch (status) {
+            default:
+            case 0:
+                statusInfo.color = colors.yellow[800];
+                statusInfo.icon = <HourglassEmptyIcon />;
+                statusInfo.label = "Pending";
+                break;
+            case 1:
+                statusInfo.color = colors.green[800];
+                statusInfo.icon = <CheckIcon />;
+                statusInfo.label = "Confirmed";
+                break;
+            case 2:
+                statusInfo.color = colors.red[800];
+                statusInfo.icon = <NotInterestedIcon />;
+                statusInfo.label = "Cancelled";
+                break;
+        }
+
+        const handleCancel = function() {
+            const data = {
+                status: 2,
+            };
+            api.put(
+                `${kBaseUrl}${id}`,
+                data,
+                enqueueSnackbar,
+                "Suborder was canceled.");
+            setUpdate(update + 1);
+        }
+
+        const handleConfirm = function() {
+            const data = {
+                status: 1,
+            };
+            api.put(
+                `${kBaseUrl}${id}`,
+                data,
+                enqueueSnackbar,
+                "Suborder was confirmed.");
+            setUpdate(update + 1);
+        }
+
+        return (
+            <Grid key={id} item xs={18} md={4}>
+                <Card variant="outlined">
+                    <CardHeader
+                        avatar={
+                            <Avatar
+                                sx={{ bgcolor: statusInfo.color }}
+                                aria-label="status-icon">
+                                {statusInfo.icon}
+                            </Avatar>
+                        }
+                        title={`${statusInfo.label} suborder`}
+                        titleTypographyProps={{ "variant": "h6" }} />
+                    <CardContent>
+                        <Grid container spacing={1} direction="column">
+                            <Grid item>
+                                <Typography variant="subtitle2">Product Unit</Typography>
+                                <Typography>{product.name} - {variant?.name}</Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="subtitle2">Quantity</Typography>
+                                <Typography>{quantity}</Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="subtitle2">Price at Checkout</Typography>
+                                <Typography>{api.currency.format(price)}</Typography>
+                            </Grid>
+                        </Grid>
+                    </CardContent>
+                    <CardActions>
+                    {status === 0 && (
+                        <>
+                            <Button onClick={handleConfirm} variant="contained" color="primary">
+                            Confirm
+                            </Button>
+                            <Button onClick={handleCancel} variant="outlined" color="primary">
+                            Cancel
+                            </Button>
+                        </>
+                    )}
+                    </CardActions>
+                </Card>
+            </Grid>
+        )
+    };
+
+    const handleOrdersGroup = function(aGroup, aIndex, aGroups) {
+        const { user, date, totalPayment, children } = aGroup;
+
+        return (
+            <Card variant="outlined">
+                <CardContent>
+                    <Stack key={aIndex} spacing={2} useFlexGap>
+                        <Typography variant="h5">Order #{aGroups.length - aIndex}: {new Date(date).toLocaleString()}</Typography>
+                        <Grid container spacing={2}>
+                            <Grid item xs={18} md={4}>
+                                <Card variant="outlined">
+                                    <CardHeader
+                                        avatar={
+                                            <Avatar
+                                                sx={{ bgcolor: colors.blue[800] }}
+                                                aria-label="status-icon">
+                                                <ListIcon />
+                                            </Avatar>
+                                        }
+                                        title="Details"
+                                        titleTypographyProps={{ "variant": "h6" }} />
+                                    <CardContent>
+                                        <Grid container spacing={1} direction="column">
+                                            <Grid item>
+                                                <Typography variant="subtitle2">Total Payment</Typography>
+                                                <Typography>{api.currency.format(totalPayment)}</Typography>
+                                            </Grid>
+                                            <Grid item>
+                                                <Typography variant="subtitle2">Total Suborders</Typography>
+                                                <Typography>{children.length}</Typography>
+                                            </Grid>
+                                        </Grid>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                            <Grid item xs={18} md={8}>
+                                <Card variant="outlined">
+                                    <CardHeader
+                                        avatar={
+                                            <Avatar
+                                                sx={{ bgcolor: colors.blue[800] }}
+                                                aria-label="status-icon">
+                                                <PlaceIcon />
+                                            </Avatar>
+                                        }
+                                        title="Delivery Address"
+                                        titleTypographyProps={{ "variant": "h6" }} />
+                                    <CardContent>
+                                        <Typography sx={{ fontWeight: "bold" }}>
+                                        {user?.firstName} {user?.middleName} {user?.lastName}
+                                        </Typography>
+                                        <Typography>
+                                        {user?.email} ({user?.username})
+                                        </Typography>
+                                        <Typography component="p">
+                                        {user?.address}
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        </Grid>
+                        <Grid container spacing={2}>
+                            {children?.map(handleOrdersChild)}
+                        </Grid>
+                    </Stack>
+                </CardContent>
+            </Card>
+        )
+    };
 
     return (
         <Container sx={{ py: 3 }}>
             <Typography variant="h4" sx={{ mb: 3 }}>Order Fulfillment</Typography>
-            <Table size="small">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>VariantID</TableCell>
-                        <TableCell>Quantity</TableCell>
-                        <TableCell>Price at Checkout</TableCell>
-                        <TableCell>UserID</TableCell>
-                        <TableCell >Date</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-
-            
-                    {orders?.map((row) => (
-                        <TableRow key={row.productId}>
-                            <TableCell>{row.variantId}</TableCell>
-                            <TableCell>{row.quantity}</TableCell>
-                            <TableCell>{row.priceAtCheckout}</TableCell>
-                            <TableCell>{row.userId}</TableCell>
-                            <TableCell>{row.date.toLocaleString()}</TableCell>
-                            <TableCell>
-                                {/* Will display the status accordingly */}
-                                {row.status === 0 ? "Pending" : row.status === 1 ? "Accomplished" : "Cancelled"}
-                            </TableCell>
-                            {/* If pednign, buttons will be returned too for the confirmation the canceling*/}
-                            {row.status === 0 && (
-                                <Box display="flex" flexDirection="column" >
-                 
-                                    <Button onClick={toConfirm} variant="contained" color="primary" style={{ marginBottom: "8px" }}>
-                    Confirm
-                                    </Button>
-                
-                                    <Button onClick={toCancel}  variant="outlined" color="primary">
-                    Cancel
-                                    </Button>
-                                </Box>
-                            )}
-
-              
-
-    
-                        </TableRow>
-                    ))}
-
-                    {/*
-
-
- id: uuidv4(),
-        productId: body.productId,
-        variantId: body.variantId,
-        quantity: body.quantity,
-        price: priceAtCheckout,
-        userId: body.userId,
-        status: body.status,
-        date: new Date()
-        
-        */}
-
-                </TableBody>
-            </Table>
-      
+            <Stack spacing={2} useFlexGap>
+                {orders?.map(handleOrdersGroup)}
+            </Stack>
         </Container>
     );
 }
