3b407bd649ec478366de2033ad2eeb914834f867
fofajardo <fofajardo@up.edu.ph>
2024-01-03 17:45:01 +0800

    feat: implement user info edit.

--- src/pages/account/page.js
+++ src/pages/account/page.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 
+import { useSnackbar } from "notistack";
 import { Link as RouterLink } from "react-router-dom";
 
 import {
     Container, Grid,
     Typography, Card, CardContent, CardActions,
-    List, ListItem, ListItemText, Button, IconButton
+    List, ListItem, ListItemText, Button, IconButton,
+    Dialog, DialogTitle, DialogContent, DialogActions,
+    TextField, useTheme, useMediaQuery
 } from "@mui/material";
 
 import {
@@ -17,8 +20,88 @@ import { getFriendlyRoleName } from "../staticTypes.js";
 
 const kBaseUrl = `${api.host}users/`;
 
+function FieldEditDialog(aProps) {
+    const { enqueueSnackbar } = useSnackbar();
+    const { onClose, dialogData, open, user } = aProps;
+    const [value, setValue] = useState("");
+
+    useEffect(function() {
+        if (!user) {
+            return;
+        }
+        if (dialogData.formName in user) {
+            setValue(user[dialogData.formName]);
+        } else {
+            setValue("");
+        }
+    }, [user, dialogData]);
+
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
+
+    const handleDialogSubmit = async function(aEvent) {
+        aEvent.preventDefault();
+
+        const formData = new FormData(aEvent.currentTarget);
+        const formJson = Object.fromEntries(formData.entries());
+
+        const result = await api.put(
+            `${kBaseUrl}${user.id}`,
+            formJson,
+            enqueueSnackbar,
+            "User information was edited successfully."
+        );
+
+        if (result.data) {
+            const refreshResult = await api.get(`${api.host}auth/refresh`);
+
+            onClose();
+        }
+    };
+
+    return (
+        <Dialog
+            id="dialog-form"
+            component="form"
+            onSubmit={handleDialogSubmit}
+            open={open}
+            onClose={onClose}
+            maxWidth="sm"
+            fullWidth
+            fullScreen={fullScreen}>
+            <DialogTitle>Edit user information</DialogTitle>
+            <DialogContent>
+                <Typography sx={{ mb: 2 }}>
+                    Your changes will take effect immediately.
+                </Typography>
+                <TextField
+                    label={dialogData.label}
+                    name={dialogData.formName}
+                    value={value}
+                    onChange={function(event) {
+                        setValue(event.target.value);
+                    }}
+                    required
+                    multiline={dialogData.formIsMulti}
+                    fullWidth />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={onClose}>Cancel</Button>
+                <Button type="submit" form="dialog-form">Save</Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
+
 export default function Account() {
     const [user, setUser] = useState(null);
+
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [dialogData, setDialogData] = useState({});
+
+    const handleClose = function() {
+        setDialogOpen(false);
+    };
 
     useEffect(function() {
         api.identify().then(function(aResult) {
@@ -27,32 +110,33 @@ export default function Account() {
             }
             setUser(aResult.data);
         });
-    }, []);
+    }, [dialogOpen]);
 
     const basicData = [
         {
             label: "First Name",
             data: user?.firstName,
-            formName: "user-first-name",
+            formName: "firstName",
             formCondition: true
         },
         {
             label: "Middle Name",
             data: user?.middleName,
-            formName: "user-middle-name",
+            formName: "middleName",
             formCondition: true
         },
         {
             label: "Last Name",
             data: user?.lastName,
-            formName: "user-last-name",
+            formName: "lastName",
             formCondition: true
         },
         {
             label: "Role",
             data: getFriendlyRoleName(user?.role),
-            formCondition: user?.role == 2,
-            formHandler: null
+            // Block role edit for now.
+            /* formCondition: user?.role == 2,
+            formHandler: null */
         },
     ];
 
@@ -60,32 +144,35 @@ export default function Account() {
         {
             label: "Username",
             data: user?.username,
-            formCondition: user?.role == 2,
-            formName: "user-username"
         },
         {
             label: "Email",
             data: user?.email,
             formCondition: true,
-            formName: "user-email"
+            formName: "email"
         },
         {
             label: "Address",
             data: user?.address,
             formCondition: true,
             formIsMulti: true,
-            formName: "user-address"
+            formName: "address"
         }
     ];
 
     const handleListItemMap = function(aItem, aIndex) {
+        const handleEdit = function(aEvent) {
+            setDialogData(aItem);
+            setDialogOpen(true);
+        };
+
         return (
             <ListItem disableGutters key={aIndex}>
                 <ListItemText primary={aItem.label} primaryTypographyProps={{ variant: "subtitle2", sx: { whiteSpace: "pre-wrap" } }} />
                 {aItem.data}
                 {
                     aItem.formCondition ? (
-                        <IconButton sx={{ ml: 1 }} size="small">
+                        <IconButton sx={{ ml: 1 }} size="small" onClick={handleEdit}>
                             <EditIcon fontSize="small" />
                         </IconButton>
                     ) : (
@@ -98,6 +185,11 @@ export default function Account() {
 
     return (
         <Container sx={{ py: 3 }}>
+            <FieldEditDialog
+                 onClose={handleClose}
+                 dialogData={dialogData}
+                 open={dialogOpen}
+                 user={user} />
             <Typography variant="h4" sx={{ mb: 1 }}>
                 My Account
             </Typography>
